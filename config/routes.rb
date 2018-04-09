5d4b9137f2654c5714fd0fae7970a1bee069b073
vboparai <vboparai11@gmail.com>
2018-04-09 19:19:07 -0400

    finance tracker

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  devise_for :users, :controllers => { :registrations => "user/registrations"}
+  resources :user_stocks, only:[:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'welcome#index'
+  get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
+
+  get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
+
+
+
+
+end
